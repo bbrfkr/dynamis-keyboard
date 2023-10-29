ce059db2d1b50082b50a94598d77270a87a789c7
Tatsunori Saito <bbrfkr@gmail.com>
2023-10-29 20:30:24 +0900

    fix default layout

--- firmware/qmk/dynamis/v2/keymaps/iso/keymap.c
+++ firmware/qmk/dynamis/v2/keymaps/iso/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
         KC_TAB,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),            KC_UP,
-        KC_LCTL, KC_LGUI, MO(1),                     KC_SPC,  KC_SPC,  KC_ENT,                    MO(2),   KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, MO(1),                     KC_SPC,  KC_SPC,  KC_ENT,                    MO(2),   KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LOWER] = LAYOUT_iso(
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_PSCR, KC_TRNS, KC_LSFT, KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_BTN1, KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-        KC_TRNS, KC_LALT, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,                   MO(3),   KC_RALT, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_LGUI, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,                   MO(3),   KC_RGUI, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [_RAISE] = LAYOUT_iso(
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-        KC_TRNS, KC_LALT, KC_TRNS,                   MO(3),   KC_TRNS, KC_TRNS,                   KC_TRNS, KC_RALT, KC_TRNS,          KC_TRNS, KC_TRNS, KC_MUTE
+        KC_TRNS, KC_LGUI, KC_TRNS,                   MO(3),   KC_TRNS, KC_TRNS,                   KC_TRNS, KC_RGUI, KC_TRNS,          KC_TRNS, KC_TRNS, KC_MUTE
     ),
 
     [_SETTINGS] = LAYOUT_iso(
